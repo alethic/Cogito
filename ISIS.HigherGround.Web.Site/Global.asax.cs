48405b0d0c5d7e06ea22f683aa31debb880c2359
Jerome Haltom <wasabi@cogito.cx>
2013-09-14 14:52:08 -0500

    New Cogito project.

--- ISIS.HigherGround.Web.Site/Global.asax.cs
+++ ISIS.HigherGround.Web.Site/Global.asax.cs
@@ -1,8 +1,16 @@
 ﻿using System.Web;
+using System.Web.Http;
 using Cogito.Application;
-using Cogito.Composition;
+using Cogito.Composition.Hosting;
 using Cogito.Web;
+using Cogito.Web.Http;
 using Cogito.Web.Mvc;
+
+using ISIS.HigherGround.Web.Site;
+
+[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(Global), "BeforeStart")]
+[assembly: WebActivatorEx.PostApplicationStartMethod(typeof(Global), "AfterStart")]
+[assembly: WebActivatorEx.ApplicationShutdownMethod(typeof(Global), "Shutdown")]
 
 namespace ISIS.HigherGround.Web.Site
 {
@@ -10,7 +18,31 @@ namespace ISIS.HigherGround.Web.Site
     public class Global : HttpApplication
     {
 
-        public static readonly ICompositionContext CompositionContext = new ApplicationCompositionContext();
+        static readonly DefaultCompositionContainer Container =
+            new DefaultCompositionContainer();
+
+        static readonly IApplicationLifecycleManager Lifecycle =
+            Container.GetExportedValue<IApplicationLifecycleManager>();
+
+        public static void BeforeStart()
+        {
+            Lifecycle.BeforeStart();
+        }
+
+        public static void Start()
+        {
+            Lifecycle.Start();
+        }
+
+        public static void AfterStart()
+        {
+            Lifecycle.AfterStart();
+        }
+
+        public static void Shutdown()
+        {
+            Lifecycle.Shutdown();
+        }
 
         /// <summary>
         /// Initializes a new instance.
@@ -19,14 +51,18 @@ namespace ISIS.HigherGround.Web.Site
             : base()
         {
             // configure MVC with container
-            this.WithComposition(CompositionContext);
-            this.WithMvcComposition(CompositionContext);
         }
 
         public void Application_Start()
         {
-            CompositionContext.GetExportedValue<IApplicationLifecycleManager>()
-                .Start();
+            this
+                .WithComposition(Container)
+                .WithMvcComposition(Container);
+
+            GlobalConfiguration.Configuration
+                .WithApiComposition(Container);
+
+            Start();
         }
 
     }
