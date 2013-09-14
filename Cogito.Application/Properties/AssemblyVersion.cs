cdd69b3a3fb52bc5c55ffc0e6e669ee756e1bc26
Jerome Haltom <wasabi@cogito.cx>
2013-09-14 18:00:58 -0500

    Stuff.

--- Cogito.Application/Properties/AssemblyVersion.cs
+++ Cogito.Application/Properties/AssemblyVersion.cs
@@ -1,4 +1,5 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("2013.9.13.0")]
-[assembly: AssemblyFileVersion("2013.9.13.0")]
+[assembly: AssemblyVersion("2013.9.14.2259")]
+[assembly: AssemblyFileVersion("2013.9.14.2259")]
+[assembly: AssemblyInformationalVersion("2013.9.14.2259")]
