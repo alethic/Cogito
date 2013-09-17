f6e85d711b423fc7a780ba7a648ff3c134a3dfe0
Jerome Haltom <wasabi@cogito.cx>
2013-09-17 08:50:42 -0500

    Nuget restore stuff Cogito build stuff.

---
+++ ISIS.HigherGround.Web.Site/Properties/AssemblyVersion.cs
@@ -0,0 +1,5 @@
+using System.Reflection;
+
+[assembly: AssemblyVersion("2013.9.17.1347")]
+[assembly: AssemblyFileVersion("2013.9.17.1347")]
+[assembly: AssemblyInformationalVersion("2013.9.17.1347")]
---
+++ ISIS.HigherGround.Web/Properties/AssemblyVersion.cs
@@ -0,0 +1,5 @@
+using System.Reflection;
+
+[assembly: AssemblyVersion("2013.9.17.1346")]
+[assembly: AssemblyFileVersion("2013.9.17.1346")]
+[assembly: AssemblyInformationalVersion("2013.9.17.1346")]
