72d1ce07def871f5f02810d97ad52d3ceaebfe10
Jerome Haltom <wasabi@cogito.cx>
2013-11-28 13:32:09 -0600

    Some tests.

--- Cogito.Nancy/NancyBootstrapper.cs
+++ Cogito.Nancy/NancyBootstrapper.cs
@@ -17,11 +17,11 @@ namespace Cogito.Nancy
     /// <summary>
     /// Base <see cref="NancyBootstrapper/> implementation which configures a Cogito composition container.
     /// </summary>
-    public abstract class NancyBootstrapper : 
+    public abstract class NancyBootstrapper :
         global::Nancy.Bootstrappers.Mef.NancyBootstrapper<Cogito.Composition.Hosting.CompositionContainerCore>
     {
 
-        protected override void RequestStartup(CompositionContainerCore container, IPipelines pipelines, NancyContext context)
+        protected override void RequestStartup(System.ComponentModel.Composition.Hosting.CompositionContainer container, IPipelines pipelines, NancyContext context)
         {
             // capture exceptions and wrap with ErrorResponse
             pipelines.OnError.AddItemToEndOfPipeline((c, e) => ErrorResponse.FromException(e));
@@ -29,29 +29,12 @@ namespace Cogito.Nancy
             base.RequestStartup(container, pipelines, context);
         }
 
-        protected override Cogito.Composition.Hosting.CompositionContainerCore GetApplicationContainer()
+        protected override System.ComponentModel.Composition.Hosting.CompositionContainer CreateRequestContainer()
         {
-            return (Cogito.Composition.Hosting.CompositionContainerCore)ContainerManager.GetDefaultContainer();
-        }
-
-        protected override Cogito.Composition.Hosting.CompositionContainerCore CreateRequestContainer()
-        {
-            return (Cogito.Composition.Hosting.CompositionContainerCore)
-                ApplicationContainer.AsContext().GetOrBeginScope<IWebRequestScope>().AsContainer();
-        }
-
-        protected override void AddCatalog(Cogito.Composition.Hosting.CompositionContainerCore container, ComposablePartCatalog catalog)
-        {
-            // find nancy export provider
-            var provider = container.Providers
-                .OfType<NancyExportProvider>()
-                .First();
-
-            // add new catalog
-            provider.Providers.Add(new CatalogExportProvider(catalog)
-            {
-                SourceProvider = container,
-            });
+            return ApplicationContainer
+                .AsContext()
+                .GetOrBeginScope<IWebRequestScope>()
+                .AsContainer();
         }
 
     }
