e9d99ca5923f9319f8ddac076735fd9773736dd7
Jerome Haltom <wasabi@cogito.cx>
2013-12-19 21:58:46 -0600

    Minor

--- Cogito.Web.UI/PageHandlerFactory.cs
+++ Cogito.Web.UI/PageHandlerFactory.cs
@@ -3,7 +3,8 @@
 namespace Cogito.Web.UI
 {
 
-    public class PageHandlerFactory : System.Web.UI.PageHandlerFactory
+    public class PageHandlerFactory :
+        System.Web.UI.PageHandlerFactory
     {
 
         public override IHttpHandler GetHandler(HttpContext context, string requestType, string virtualPath, string path)
