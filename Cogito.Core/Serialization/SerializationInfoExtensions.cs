ecfc0675fb435b10be9dcde9594e24145acbf632
Jerome Haltom <jhaltom@isillc.com>
2014-06-25 10:42:11 -0500

    FontReplacingSmartCopy, which doesn't work. Updated some NuGet packages. XfdDocumentTemplateBinder: Embed true type fonts without subsetting. PdfDocumentComposer: upgrade PDF version to 1.7 Some basic disabled Dedup of array logic in CampaignJobEngine.

---
+++ Cogito.Core/Serialization/SerializationInfoExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using System.Runtime.Serialization;
+
+namespace Cogito.Serialization
+{
+
+    public static class SerializationInfoExtensions
+    {
+
+        public static T GetValue<T>(this SerializationInfo info, string name)
+        {
+            Contract.Requires<ArgumentNullException>(info != null);
+            Contract.Requires<ArgumentNullException>(name != null);
+
+            return (T)info.GetValue(name, typeof(T));
+        }
+
+    }
+
+}
