5dfa421bca5688f5b62a484f1f7dd86b585988cf
Jerome Haltom <jhaltom@isillc.com>
2014-06-26 14:33:12 -0500

    Added Browser page, and some other stuff.

---
+++ Cogito.Web.UI/Resources/ScriptResourceBundleReference.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using System.Web.UI;
+
+using Cogito.Resources;
+
+namespace Cogito.Web.UI.Resources
+{
+
+    public class ScriptResourceBundleReference :
+        ScriptReference
+    {
+
+        readonly IResourceBundle bundle;
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="bundle"></param>
+        /// <param name="name"></param>
+        public ScriptResourceBundleReference(IResourceBundle bundle, string name)
+        {
+            Contract.Requires<ArgumentNullException>(bundle != null);
+            Contract.Requires<ArgumentNullException>(name != null);
+
+            this.Name = name;
+            this.bundle = bundle;
+        }
+
+    }
+
+}
--- Cogito.Web.UI/Resources/ScriptResourcePageInstaller.cs
+++ Cogito.Web.UI/Resources/ScriptResourcePageInstaller.cs
@@ -53,7 +53,7 @@ namespace Cogito.Web.UI.Resources
         /// </summary>
         /// <param name="resource"></param>
         /// <returns></returns>
-        string GetResourceName(IResource resource)
+        string GetResourceNameFromScript(IResource resource)
         {
             Contract.Requires<ArgumentNullException>(resource != null);
 
@@ -62,6 +62,49 @@ namespace Cogito.Web.UI.Resources
                 name = name.RemoveEnd(".js");
 
             return name;
+        }
+
+        /// <summary>
+        /// Gets the resource name for the given resource.
+        /// </summary>
+        /// <param name="resource"></param>
+        /// <returns></returns>
+        string GetResourceName(IResource resource)
+        {
+            // does the resource specify any specific script resource information?
+            var web = resource as IWebScriptResource;
+            var src = web != null ? web.ScriptResourceName : GetResourceNameFromScript(resource);
+
+            return src;
+        }
+
+        /// <summary>
+        /// Gets the resource name for a bundle.
+        /// </summary>
+        /// <param name="bundle"></param>
+        /// <returns></returns>
+        string GetBundleName(IResourceBundle bundle)
+        {
+            var web = bundle as IWebScriptResourceBundle;
+            var src = web != null ? web.ScriptResourceName : null;
+            if (src == null)
+            {
+                var scr = bundle.FirstOrDefault(i => i.ContentType == "application/javascript");
+                if (scr != null)
+                    src = GetResourceName(scr);
+            }
+
+            return src;
+        }
+
+        /// <summary>
+        /// Gets whether or not the resource is a debug resource.
+        /// </summary>
+        /// <param name="resource"></param>
+        /// <returns></returns>
+        bool IsDebug(IResource resource)
+        {
+            return resource.IsDebug ?? false;
         }
 
         /// <summary>
@@ -78,26 +121,94 @@ namespace Cogito.Web.UI.Resources
             if (resource.ContentType != "application/javascript")
                 return false;
 
+            // install everything in the bundle
+            if (resource.Bundle != null)
+                return InstallBundle(page, resource.Bundle);
+
             var url = ResolveUrl(page, resource);
             if (url == null)
                 throw new NullReferenceException(string.Format("Could not resolve Url for resource {0}.", resource));
 
-            // does the resource specify any specific script resource information?
-            var web = resource as IWebScriptResource;
-            var src = web != null ? web.ScriptResourceName : GetResourceName(resource);
-
             // define resource
+            var src = GetResourceName(resource);
             var def = new ScriptResourceDefinition();
             def.Path = url;
             ScriptManager.ScriptResourceMapping.AddDefinition(src, null, def);
 
             // register current script
-            ScriptManager.GetCurrent(page).Scripts.Add(new ScriptReference()
+            ScriptManager.GetCurrent(page).Scripts.Insert(0, new ScriptReference()
             {
                 Name = src,
             });
 
             return true;
+        }
+
+        /// <summary>
+        /// Installs all the resources in a given bundle.
+        /// </summary>
+        /// <param name="page"></param>
+        /// <param name="bundle"></param>
+        /// <returns></returns>
+        bool InstallBundle(Page page, IResourceBundle bundle)
+        {
+            var src = GetBundleName(bundle);
+            if (src == null)
+                return false;
+
+            var def = GetScriptDefinition(page, bundle);
+            if (def == null)
+                return false;
+
+            // check for existing mapping
+            var map = ScriptManager.ScriptResourceMapping.GetDefinition(src);
+            if (map != null &&
+                map.Path == def.Path &&
+                map.DebugPath == def.DebugPath)
+                return true;
+
+            // add the resource mapping, that didn't already exist
+            ScriptManager.ScriptResourceMapping.AddDefinition(src, null, def);
+
+            // insert script reference into page
+            var lst = ScriptManager.GetCurrent(page).Scripts;
+            var pos = lst.Select((i, j) => !(i is ScriptResourceBundleReference) ? j : -1).FirstOrDefault(i => i > -1);
+            lst.Insert(pos, new ScriptResourceBundleReference(bundle, src));
+
+            return true;
+        }
+
+        /// <summary>
+        /// Generates a script definition for the given bundle.
+        /// </summary>
+        /// <param name="page"></param>
+        /// <param name="bundle"></param>
+        /// <returns></returns>
+        ScriptResourceDefinition GetScriptDefinition(Page page, IResourceBundle bundle)
+        {
+            var scripts = bundle
+                .Where(i => i.ContentType == "application/javascript");
+
+            // script for non-debug mode
+            var script = scripts
+                .Where(i => !IsDebug(i))
+                .FirstOrDefault();
+
+            // script for debug mode
+            var debugScript = scripts
+                .Where(i => IsDebug(i))
+                .FirstOrDefault();
+
+            // generate new definition if paths are available
+            if (script != null ||
+                debugScript != null)
+                return new ScriptResourceDefinition()
+                {
+                    Path = script != null ? ResolveUrl(page, script) : null,
+                    DebugPath = debugScript != null ? ResolveUrl(page, debugScript) : null,
+                };
+
+            return null;
         }
 
     }
