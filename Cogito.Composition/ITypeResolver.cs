3ea7b967148f104f3a3a24a6184dfe6bce85cb43
Jerome Haltom <jhaltom@isillc.com>
2014-09-09 13:42:16 -0500

    Added simple TypeResolver.

---
+++ Cogito.Composition/ITypeResolver.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Cogito.Composition
+{
+
+    public interface ITypeResolver
+    {
+
+        T Resolve<T>();
+
+        Lazy<T, IDictionary<string, object>> ResolveLazy<T>();
+
+        IEnumerable<T> ResolveMany<T>();
+
+        IEnumerable<Lazy<T,IDictionary<string,object>>> ResolveManyLazy<T>();
+
+        object Resolve(Type type);
+
+        Lazy<object, IDictionary<string, object>> ResolveLazy(Type type);
+
+        IEnumerable<object> ResolveMany(Type type);
+
+        IEnumerable<Lazy<object, IDictionary<string, object>>> ResolveManyLazy(Type type);
+
+    }
+
+}
