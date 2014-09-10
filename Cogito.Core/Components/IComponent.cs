8b63b88539ae3f0e0d5e84a410c7218e8933a6eb
Jerome Haltom <jhaltom@isillc.com>
2014-09-10 10:50:14 -0500

    Added Component, Service and ServiceBus implementations. As well as some threading primitives.

---
+++ Cogito.Core/Components/IComponent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Cogito.Components
+{
+
+    /// <summary>
+    /// Describes a component with lifetime management.
+    /// </summary>
+    public interface IComponent :
+        IDisposable
+    {
+
+        /// <summary>
+        /// Adds a <see cref="IDisposable"/> to be disposed when the component is disposed.
+        /// </summary>
+        /// <param name="disposable"></param>
+        void Attach(IDisposable disposable);
+
+    }
+
+}
---
+++ Cogito.ServiceBus/Infrastructure/SemaphoreStatus.cs
@@ -0,0 +1,22 @@
+﻿namespace Cogito.ServiceBus.Infrastructure
+{
+
+    /// <summary>
+    /// Indicates the status of a semaphore node.
+    /// </summary>
+    public enum SemaphoreStatus
+    {
+
+        /// <summary>
+        /// The semaphore node is available.
+        /// </summary>
+        Available = 1,
+
+        /// <summary>
+        /// The semaphore node is unavailable.
+        /// </summary>
+        Unavailable = 2,
+
+    }
+
+}
