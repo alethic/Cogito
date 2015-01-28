feb4a915aa2cb2ef7be6799591a23e3da3052fa0
Jerome Haltom <jhaltom@isillc.com>
2015-01-28 16:39:25 -0600

    Renamed WithClass to AddClass. New WebControl specific extensions.

--- Cogito.Web.UI/ControlExtensions.cs
+++ Cogito.Web.UI/ControlExtensions.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics.Contracts;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Web.UI;
+
+using Cogito.Linq;
 
 namespace Cogito.Web.UI
 {
@@ -68,19 +71,58 @@ namespace Cogito.Web.UI
         }
 
         /// <summary>
-        /// Adds the given CSS class to the <see cref="Control"/>.
+        /// Adds the given CSS class to the <see cref="IAttributeAccessor"/>.
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <param name="control"></param>
         /// <param name="class"></param>
         /// <returns></returns>
-        public static T WithClass<T>(this T control, string @class)
+        public static T AddClass<T>(this T control, string @class)
             where T : IAttributeAccessor
         {
             Contract.Requires<ArgumentNullException>(control != null);
 
             if (!string.IsNullOrWhiteSpace(@class))
-                control.SetAttribute("class", (control.GetAttribute("class") + " " + @class).TrimEnd());
+            {
+                // split all class names, append new class name, remove duplicates
+                var css = control.GetAttribute("class")
+                    .Split(' ')
+                    .Append(@class)
+                    .Select(i => i.TrimOrNull())
+                    .Where(i => i != null)
+                    .ToArray();
+
+                if (css.Length > 0)
+                    control.SetAttribute("class", string.Join(" ", css));
+            }
+
+            return control;
+        }
+
+        /// <summary>
+        /// Removes the given CSS class from the <see cref="IAttributeAccessor"/>.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="control"></param>
+        /// <param name="class"></param>
+        /// <returns></returns>
+        public static T RemoveClass<T>(this T control, string @class)
+            where T : IAttributeAccessor
+        {
+            Contract.Requires<ArgumentNullException>(control != null);
+
+            if (!string.IsNullOrWhiteSpace(@class))
+            {
+                // split all class names, append new class name, remove duplicates
+                var css = control.GetAttribute("class")
+                    .Split(' ')
+                    .Select(i => i.TrimOrNull())
+                    .Where(i => i != null)
+                    .Where(i => i != @class)
+                    .ToArray();
+
+                control.SetAttribute("class", string.Join(" ", css));
+            }
 
             return control;
         }
---
+++ Cogito.Web.UI/WebControlExtensions.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Web.UI.WebControls;
+
+using Cogito.Linq;
+
+namespace Cogito.Web.UI
+{
+
+    /// <summary>
+    /// Provides extensions that make working with <see cref="WebControl"/> instances easier.
+    /// </summary>
+    public static class WebControlExtensions
+    {
+
+        /// <summary>
+        /// Adds the given CSS class to the <see cref="WebControl"/>.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="control"></param>
+        /// <param name="class"></param>
+        /// <returns></returns>
+        public static T AddCssClass<T>(this T control, string @class)
+            where T : WebControl
+        {
+            Contract.Requires<ArgumentNullException>(control != null);
+
+            if (!string.IsNullOrWhiteSpace(@class))
+            {
+                // split all class names, append new class name, remove duplicates
+                var css = control.CssClass
+                    .Split(' ')
+                    .Append(@class)
+                    .Select(i => i.TrimOrNull())
+                    .Where(i => i != null)
+                    .ToArray();
+
+                if (css.Length > 0)
+                    control.CssClass = string.Join(" ", css);
+            }
+
+            return control;
+        }
+
+        /// <summary>
+        /// Removes the given CSS class from the <see cref="WebControl"/>.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="control"></param>
+        /// <param name="class"></param>
+        /// <returns></returns>
+        public static T RemoveCssClass<T>(this T control, string @class)
+            where T : WebControl
+        {
+            Contract.Requires<ArgumentNullException>(control != null);
+
+            if (!string.IsNullOrWhiteSpace(@class))
+            {
+                // split all class names, append new class name, remove duplicates
+                var css = control.CssClass
+                    .Split(' ')
+                    .Select(i => i.TrimOrNull())
+                    .Where(i => i != null)
+                    .Where(i => i != @class)
+                    .ToArray();
+
+                control.CssClass = string.Join(" ", css);
+            }
+
+            return control;
+        }
+
+    }
+
+}
