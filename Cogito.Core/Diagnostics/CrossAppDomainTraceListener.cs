4cadfe93c4f39aef9ee0d37508d8c9c3edfcc445
Jerome Haltom <jhaltom@isillc.com>
2015-03-06 16:51:32 -0600

    Rename basePath and tempPath to binPath and tmpPath. Added TraceListener infrastructure to relay messages from child domains.

---
+++ Cogito.Core/Diagnostics/CrossAppDomainTraceListener.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
+
+namespace Cogito.Core.Diagnostics
+{
+
+    public class CrossAppDomainTraceListener :
+        MarshalByRefObject
+    {
+
+        CrossAppDomainTraceListener parent;
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        public CrossAppDomainTraceListener()
+        {
+
+        }
+
+        /// <summary>
+        /// Attaches to the remote diagnostics pipeline and forwards messages to the current <see cref="AppDomain"/>.
+        /// </summary>
+        /// <param name="domain"></param>
+        public static void ListenTo(AppDomain domain)
+        {
+            var listenerType = typeof(CrossAppDomainTraceListener);
+            var remote = (CrossAppDomainTraceListener)domain.CreateInstanceAndUnwrap(
+                listenerType.Assembly.FullName,
+                listenerType.FullName);
+
+            var local = new CrossAppDomainTraceListener();
+
+            remote.Register(local);
+        }
+
+        /// <summary>
+        /// Invoked on the remote <see cref="AppDomain"/>.
+        /// </summary>
+        /// <param name="parent"></param>
+        void Register(CrossAppDomainTraceListener parent)
+        {
+            Contract.Requires<ArgumentNullException>(parent != null);
+
+            this.parent = parent;
+
+            // append a new trace listener that invokes the local Write method
+            Trace.Listeners.Add(new DelegateTraceListener(Write));
+        }
+
+        /// <summary>
+        /// Invoked on the remote <see cref="AppDomain"/> to write a message.
+        /// </summary>
+        /// <param name="message"></param>
+        void Write(string message)
+        {
+            Contract.Requires<ArgumentNullException>(message != null);
+
+            parent.RemoteWrite(message);
+        }
+
+        /// <summary>
+        /// Invoked on the local <see cref="AppDomain"/> to write a message.
+        /// </summary>
+        /// <param name="message"></param>
+        void RemoteWrite(string message)
+        {
+            Contract.Requires<ArgumentNullException>(message != null);
+
+            Trace.Write(message);
+        }
+
+    }
+
+}
---
+++ Cogito.Core/Diagnostics/DelegateTraceListener.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
+
+namespace Cogito.Core.Diagnostics
+{
+
+    /// <summary>
+    /// Provides a <see cref="TraceListener"/> implementation that writes messages to a delegate.
+    /// </summary>
+    public class DelegateTraceListener :
+        TraceListener
+    {
+
+        Action<string> _write;
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="write"></param>
+        public DelegateTraceListener(Action<string> write)
+        {
+            Contract.Requires<ArgumentNullException>(write != null);
+
+            _write = write;
+        }
+
+        public override void Write(string message)
+        {
+            _write(message);
+        }
+
+        public override void WriteLine(string message)
+        {
+            Write(message + Environment.NewLine);
+        }
+
+    }
+
+}
