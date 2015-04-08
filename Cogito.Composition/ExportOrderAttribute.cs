5d07a36e8a4a42b93dd2942b16369540e0f707c8
Jerome Haltom <wasabi@cogito.cx>
2015-04-08 00:03:06 -0500

    Order metadata and ordered importer.

---
+++ Cogito.Composition/ExportOrderAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.Composition;
+
+namespace Cogito.Composition
+{
+
+    /// <summary>
+    /// Attaches an Order metadata property to the export.
+    /// </summary>
+    [MetadataAttribute]
+    public class ExportOrderAttribute :
+        Attribute
+    {
+
+        readonly int order;
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="order"></param>
+        /// <returns></returns>
+        public ExportOrderAttribute(int order)
+        {
+            Priority = order;
+        }
+
+        public int Priority { get; set; }
+
+    }
+
+}
---
+++ Cogito.Composition/OrderedManyImport.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Diagnostics.Contracts;
+using System.Linq;
+
+using Cogito.Linq;
+
+namespace Cogito.Composition
+{
+
+    /// <summary>
+    /// Provides a set of the specified type ordered by the order metadata.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    [Export(typeof(OrderedImportMany<>))]
+    public class OrderedImportMany<T> :
+        IEnumerable<T>
+    {
+
+        readonly IEnumerable<Lazy<T, IOrderedExportMetadata>> imports;
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="imports"></param>
+        [ImportingConstructor]
+        public OrderedImportMany(
+            [ImportMany] IEnumerable<Lazy<T, IOrderedExportMetadata>> imports)
+        {
+            Contract.Requires<ArgumentNullException>(imports != null);
+
+            this.imports = imports.OrderBy(i => i.Metadata.Order).Tee();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return imports.Select(i => i.Value).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+    }
+
+}
