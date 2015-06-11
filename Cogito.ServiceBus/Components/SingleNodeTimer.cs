3b598c8e582a4e2999b28eeedf9bf1befd5c4095
Jerome Haltom <jhaltom@isillc.com>
2015-06-10 23:21:26 -0500

    SingleNodeTimer now has default interval values. SingleNodeTimer now supports repeat and retry interval values. When an error happens, the error interval is used to schedule the next run. When a Repeat is specified (by the Repeat method), the repeat interval is used for the next run.

--- Cogito.ServiceBus/Components/SingleNodeTimer.cs
+++ Cogito.ServiceBus/Components/SingleNodeTimer.cs
@@ -18,27 +18,44 @@ namespace Cogito.ServiceBus.Components
         where TComponent : IComponent
     {
 
+        static readonly TimeSpan DEFAULT_INTERVAL = TimeSpan.FromMinutes(5);
+        static readonly TimeSpan DEFAULT_INITIAL_INTERVAL = TimeSpan.FromSeconds(5);
+        static readonly TimeSpan DEFAULT_RETRY_INTERVAL = TimeSpan.FromMinutes(1);
+        static readonly TimeSpan DEFAULT_REPEAT_INTERVAL = TimeSpan.FromSeconds(2);
+
         readonly object sync = new object();
-        readonly TimeSpan interval;
+        TimeSpan initialInterval;
+        TimeSpan interval;
+        TimeSpan retryInterval;
+        TimeSpan repeatInterval;
         System.Timers.Timer timer;
         CancellationTokenSource cts;
+        bool repeat;
+        bool retry;
         bool disposed;
 
         /// <summary>
         /// Initializes a new instance.
         /// </summary>
         /// <param name="manager"></param>
-        /// <param name="bus"></param>
-        /// <param name="data"></param>
+        /// <param name="interval"></param>
+        /// <param name="initialInterval"></param>
+        /// <param name="retryInterval"></param>
+        /// <param name="repeatInterval"></param>
         public SingleNodeTimer(
             SingleNodeTimerManager<TComponent> manager,
-            TimeSpan interval)
+            TimeSpan? interval = null,
+            TimeSpan? initialInterval = null,
+            TimeSpan? retryInterval = null,
+            TimeSpan? repeatInterval = null)
             : base(manager)
         {
             Contract.Requires<ArgumentNullException>(manager != null);
-            Contract.Requires<ArgumentOutOfRangeException>(interval.TotalMilliseconds > 0);
 
-            this.interval = interval;
+            this.interval = interval ?? DEFAULT_INTERVAL;
+            this.initialInterval = initialInterval ?? DEFAULT_INITIAL_INTERVAL;
+            this.retryInterval = retryInterval ?? DEFAULT_RETRY_INTERVAL;
+            this.repeatInterval = repeatInterval ?? DEFAULT_REPEAT_INTERVAL;
         }
 
         protected override void OnStart()
@@ -55,7 +72,7 @@ namespace Cogito.ServiceBus.Components
 
                 cts = new CancellationTokenSource();
                 timer = new System.Timers.Timer();
-                timer.Interval = interval.TotalMilliseconds;
+                timer.Interval = initialInterval.TotalMilliseconds;
                 timer.AutoReset = false;
                 timer.Elapsed += timer_Elapsed;
                 timer.Start();
@@ -92,6 +109,10 @@ namespace Cogito.ServiceBus.Components
         {
             lock (sync)
             {
+                // by default do not do fast repeat
+                repeat = false;
+                retry = false;
+
                 try
                 {
                     Trace.TraceInformation("{0}: OnTimer (entered)", GetType().Name);
@@ -104,16 +125,32 @@ namespace Cogito.ServiceBus.Components
                 }
                 catch (Exception e)
                 {
-                    Trace.TraceInformation("{0}: OnTimer (error)", GetType().Name, e);
                     OnException(e);
                 }
                 finally
                 {
                     // resume timer
                     if (timer != null)
+                    {
+                        timer.Interval = GetNextInterval().TotalMilliseconds;
                         timer.Start();
+                    }
                 }
             }
+        }
+
+        /// <summary>
+        /// Gets the appropriate <see cref="TimeSpan"/> for the next interval based on the current state.
+        /// </summary>
+        /// <returns></returns>
+        TimeSpan GetNextInterval()
+        {
+            if (repeat)
+                return repeatInterval;
+            else if (retry)
+                return retryInterval;
+            else
+                return interval;
         }
 
         /// <summary>
@@ -128,12 +165,30 @@ namespace Cogito.ServiceBus.Components
         /// <summary>
         /// Invoked when an <see cref="Exception"/> is thrown by the timer.
         /// </summary>
-        /// <param name="e"></param>
-        protected virtual void OnException(Exception e)
+        /// <param name="exception"></param>
+        protected virtual void OnException(Exception exception)
         {
-            Contract.Requires<ArgumentNullException>(e != null);
+            Contract.Requires<ArgumentNullException>(exception != null);
 
-            e.Trace();
+            exception.Trace();
+
+            Retry();
+        }
+
+        /// <summary>
+        /// Invoked from with the timer method to cause the timer to repeat on the repeat interval.
+        /// </summary>
+        protected void Repeat()
+        {
+            repeat = true;
+        }
+
+        /// <summary>
+        /// Invoked from with the timer method to cause the timer to repeat on the retry interval.
+        /// </summary>
+        protected void Retry()
+        {
+            retry = true;
         }
 
         /// <summary>
