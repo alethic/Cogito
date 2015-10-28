9d1d7804daee4b81fab85f2e93fdfd1b0b30d6ff
Jerome Haltom <wasabi@cogito.cx>
2015-10-27 19:33:55 -0500

    Fix Razor tests broken by change to full string passing.

--- Cogito.Web.Razor.Tests/RazorTemplateBuilderTests.cs
+++ Cogito.Web.Razor.Tests/RazorTemplateBuilderTests.cs
@@ -18,14 +18,14 @@ namespace Cogito.Web.Razor.Tests
         [TestMethod]
         public void Test_simple_code_generation()
         {
-            var t = RazorTemplateBuilder.ToCode(() => LoadTemplateText("Simple.cshtml"));
+            var t = RazorTemplateBuilder.ToCode(LoadTemplateText("Simple.cshtml").ReadToEnd());
             Assert.IsTrue(t.Contains(@"@__CompiledTemplate"));
         }
 
         [TestMethod]
         public void Test_simple_helper_code_generation()
         {
-            var t = RazorTemplateBuilder.ToCode(() => LoadTemplateText("SimpleWithHelper.cshtml"));
+            var t = RazorTemplateBuilder.ToCode(LoadTemplateText("SimpleWithHelper.cshtml").ReadToEnd());
             Assert.IsTrue(t.Contains(@"@__CompiledTemplate"));
         }
 
