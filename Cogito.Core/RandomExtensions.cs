fe814e36618a178e0470818d3b6f24b8dd0b664d
Jerome Haltom <jhaltom@cloverlux.com>
2015-11-30 20:09:46 -0600

    Added an extension for generating random longs.

---
+++ Cogito.Core/RandomExtensions.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Cogito
+{
+
+    public static class RandomExtensions
+    {
+
+        /// <summary>
+        /// Gets the next random <see cref="Int64"/>.
+        /// </summary>
+        /// <param name="self"></param>
+        /// <returns></returns>
+        public static long NextInt64(this Random self)
+        {
+            var buffer = new byte[8];
+            self.NextBytes(buffer);
+            return BitConverter.ToInt64(buffer, 0);
+        }
+
+        /// <summary>
+        /// Gets the next random <see cref="Int64"/> within the specified range.
+        /// </summary>
+        /// <param name="rnd"></param>
+        /// <param name="min"></param>
+        /// <param name="max"></param>
+        /// <returns></returns>
+        public static long NextInt64(this Random rnd, long min, long max)
+        {
+            EnsureMinLEQMax(ref min, ref max);
+
+            var numbersInRange = unchecked(max - min + 1);
+            if (numbersInRange < 0)
+                throw new ArgumentException("Size of range between min and max must be less than or equal to Int64.MaxValue");
+
+            var randomOffset = NextInt64(rnd);
+            if (IsModuloBiased(randomOffset, numbersInRange))
+                return NextInt64(rnd, min, max); // Try again
+            else
+                return min + PositiveModuloOrZero(randomOffset, numbersInRange);
+        }
+
+        static bool IsModuloBiased(long randomOffset, long numbersInRange)
+        {
+            var greatestCompleteRange = numbersInRange * (long.MaxValue / numbersInRange);
+            return randomOffset > greatestCompleteRange;
+        }
+
+        static long PositiveModuloOrZero(long dividend, long divisor)
+        {
+            long mod;
+            Math.DivRem(dividend, divisor, out mod);
+            if (mod < 0)
+                mod += divisor;
+            return mod;
+        }
+
+        static void EnsureMinLEQMax(ref long min, ref long max)
+        {
+            if (min <= max)
+                return;
+
+            var temp = min;
+            min = max;
+            max = temp;
+        }
+
+    }
+
+}
