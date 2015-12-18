610f87e0212a4fe43945b9f03923b478b9bb6201
Jerome Haltom <jhaltom@cloverlux.com>
2015-12-18 16:28:29 -0600

    Missing stuff.

---
+++ Cogito.Core.Tests/Linq/EnumerableExtensionsTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+using Cogito.Linq;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Cogito.Core.Tests.Linq
+{
+
+    [TestClass]
+    public class EnumerableExtensionsTests
+    {
+
+        [TestMethod]
+        public void Test_GroupAdjacent()
+        {
+            var l = new[]
+            {
+                Tuple.Create("Name1", "Value1"),
+                Tuple.Create("Name1", "Value1"),
+                Tuple.Create("Name2", "Value1"),
+                Tuple.Create("Name2", "Value1"),
+                Tuple.Create("Name1", "Value2"),
+                Tuple.Create("Name1", "Value2"),
+            };
+
+            var a = l.GroupAdjacent(i => i.Item1).ToList();
+
+            Assert.AreEqual(3, a.Count);
+        }
+
+    }
+
+}
