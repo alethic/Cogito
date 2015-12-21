3f2e372fc5c41e9cb052a0e3e0cfe2169a8e4edb
Jerome Haltom <jhaltom@cloverlux.com>
2015-12-21 15:28:09 -0600

    Working with new Actor timer stuff.

---
+++ Cogito.Activities.Tests/ActionActivityTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Activities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Cogito.Activities.Tests
+{
+
+    [TestClass]
+    public class ActionActivityTests
+    {
+
+        [TestMethod]
+        public void TestActionActivity()
+        {
+            var c = false;
+            var a = new ActionActivity(ctx => c = true);
+            var b = WorkflowInvoker.Invoke(a);
+            Assert.AreEqual(true, c);
+        }
+
+    }
+
+}
---
+++ Cogito.Activities.Tests/FuncActivityTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Activities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Cogito.Activities.Tests
+{
+
+    [TestClass]
+    public class FuncActivityTests
+    {
+
+        [TestMethod]
+        public void TestFuncActivity()
+        {
+            var a = new FuncActivity<bool>(ctx => true);
+            var b = WorkflowInvoker.Invoke(a);
+            Assert.AreEqual(true, b);
+        }
+
+    }
+
+}
