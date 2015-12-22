d4872cdb8e4cde52c6481a852eaa2674ab5e0368
Jerome Haltom <jhaltom@cloverlux.com>
2015-12-21 20:11:19 -0600

    Retry activity. Retries a number of times. Then returns up the total set of exceptions. Unsure of the persistence requirements of this. Will need to examine.

---
+++ Cogito.Activities.Tests/RetryTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Linq;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Cogito.Activities.Tests
+{
+
+    [TestClass]
+    public class RetryTests
+    {
+
+        /// <summary>
+        /// Tests for complete failure.
+        /// </summary>
+        [TestMethod]
+        public void Test_Retry_Failure()
+        {
+            int runCount = 0;
+
+            try
+            {
+                WorkflowInvoker.Invoke(new Retry()
+                {
+                    MaxAttempts = 5,
+                    Body = Activities.Action(() => { runCount++; throw new Exception("Exception"); }),
+                });
+            }
+            catch (RetryException e)
+            {
+                Assert.AreEqual(5, e.InnerExceptions.Count);
+                Assert.AreEqual(5, runCount);
+
+                return;
+            }
+
+            Assert.Fail();
+        }
+
+
+        [TestMethod]
+        public void Test_Retry_Finally()
+        {
+            int runCount = 0;
+            var results = WorkflowInvoker.Invoke(new Retry()
+            {
+                MaxAttempts = 5,
+                Body = Activities.Action(() => { if (++runCount < 3) throw new Exception("Exception"); }),
+            });
+
+            Assert.AreEqual(3, runCount);
+            Assert.AreEqual(2, ((IEnumerable<Exception>)results["Exceptions"]).ToArray().Length);
+        }
+
+        [TestMethod]
+        public void Test_Retry_Success()
+        {
+            var results = WorkflowInvoker.Invoke(new Retry()
+            {
+                MaxAttempts = 5,
+                Body = Activities.Action(() => { }),
+            });
+
+            Assert.AreEqual(1, (int)results["Attempts"]);
+        }
+
+    }
+}
+
---
+++ Cogito.Activities/Retry.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace Cogito.Activities
+{
+
+    public class Retry :
+        NativeActivity
+    {
+
+        Variable<int> attempts;
+        Variable<List<Exception>> exceptions;
+
+        /// <summary>
+        /// Number of tries to attempt.
+        /// </summary>
+        [DefaultValue(1)]
+        public InArgument<int> MaxAttempts { get; set; }
+
+        /// <summary>
+        /// Number of attempts that were made.
+        /// </summary>
+        public OutArgument<int> Attempts { get; set; }
+
+        /// <summary>
+        /// Body to execute.
+        /// </summary>
+        [RequiredArgument]
+        public Activity Body { get; set; }
+
+        /// <summary>
+        /// Aggregate of exceptions that occurred during retry.
+        /// </summary>
+        public OutArgument<IEnumerable<Exception>> Exceptions { get; set; }
+
+        /// <summary>
+        /// Executed when an exception occurs.
+        /// </summary>
+        public ActivityAction<Exception> OnException { get; set; }
+
+        /// <summary>
+        /// Creates and validates a description of the <see cref="Activity"/>.
+        /// </summary>
+        /// <param name="metadata"></param>
+        protected override void CacheMetadata(NativeActivityMetadata metadata)
+        {
+            base.CacheMetadata(metadata);
+            metadata.AddImplementationVariable(attempts = new Variable<int>());
+            metadata.AddImplementationVariable(exceptions = new Variable<List<Exception>>());
+        }
+
+        protected override bool CanInduceIdle
+        {
+            get { return true; }
+        }
+
+        /// <summary>
+        /// Executes the <see cref="Activity"/>.
+        /// </summary>
+        /// <param name="context"></param>
+        protected override void Execute(NativeActivityContext context)
+        {
+            context.SetValue(attempts, 0);
+            context.SetValue(exceptions, new List<Exception>());
+            TryScheduleBody(context);
+        }
+
+        /// <summary>
+        /// Invoked when the body completes.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="instance"></param>
+        void OnBodyCompleted(NativeActivityContext context, ActivityInstance instance)
+        {
+            if (instance.State != ActivityInstanceState.Faulted)
+                BeforeExit(context);
+        }
+
+        /// <summary>
+        /// Invoked when an error occurs in the body.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="propagatedException"></param>
+        /// <param name="propagatedFrom"></param>
+        void OnBodyFaulted(NativeActivityFaultContext context, Exception propagatedException, ActivityInstance propagatedFrom)
+        {
+            // clear out children faults, we will expose our own
+            context.HandleFault();
+            context.CancelChild(propagatedFrom);
+
+            // record exception
+            exceptions.Get(context).Add(propagatedException);
+
+            // if user wants to be notified upon an exception
+            if (OnException != null)
+            {
+                // dispatch to exception handler, which will handle, and then retry the body
+                context.ScheduleAction(OnException, propagatedException, OnExceptionCompleted);
+                return;
+            }
+
+            // schedule the body to execute again
+            TryScheduleBody(context);
+        }
+
+        /// <summary>
+        /// Invoked when the exception handler is complete.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="completedInstance"></param>
+        void OnExceptionCompleted(NativeActivityContext context, ActivityInstance completedInstance)
+        {
+            TryScheduleBody(context);
+        }
+
+        /// <summary>
+        /// Schedules the body to execute, after decrementing the attempts counter
+        /// </summary>
+        /// <param name="context"></param>
+        void TryScheduleBody(NativeActivityContext context)
+        {
+            if (attempts.Get(context) < MaxAttempts.Get(context))
+            {
+                context.SetValue(attempts, context.GetValue(attempts) + 1);
+                context.ScheduleActivity(Body, OnBodyCompleted, OnBodyFaulted);
+            }
+            else
+            {
+                BeforeExit(context);
+
+                // throw exception if there are any
+                var l = exceptions.Get(context);
+                if (l.Count > 0)
+                    throw new RetryException(attempts.Get(context), l);
+            }
+        }
+
+        /// <summary>
+        /// Executes just before the activity is to be closed.
+        /// </summary>
+        /// <param name="context"></param>
+        void BeforeExit(NativeActivityContext context)
+        {
+            context.SetValue(Attempts, context.GetValue(attempts));
+            context.SetValue(Exceptions, exceptions.Get(context));
+        }
+
+    }
+
+    public class Retry<TResult> :
+        NativeActivity<TResult>
+    {
+
+        protected override void Execute(NativeActivityContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+
+}
