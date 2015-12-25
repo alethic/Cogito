5eaa231167a1a39c64d688ef29ddb5c7cdbeb026
Jerome Haltom <jhaltom@cloverlux.com>
2015-12-25 12:42:43 -0600

    Using.

--- Cogito.Activities/AsyncTaskCodeActivity.cs
+++ Cogito.Activities/AsyncTaskCodeActivity.cs
@@ -3,7 +3,7 @@ using System.Activities;
 using System.Runtime.ExceptionServices;
 using System.Threading;
 using System.Threading.Tasks;
-using Cogito.Core.Threading;
+
 using Cogito.Threading;
 
 namespace Cogito.Activities
