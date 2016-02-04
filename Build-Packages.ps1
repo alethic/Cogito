d22b1a1bce0f326c1d2da93f38d411c458a40d68
Jerome Haltom <wasabi@cogito.cx>
2016-02-04 11:04:26 -0600

    foo

--- Build-Packages.ps1
+++ Build-Packages.ps1
@@ -1,14 +1,77 @@
-$nuget = "$($env:AGENT_HOMEDIRECTORY)\Worker\tools\NuGet.exe"
-if (!(Test-Path $nuget))
+param(
+
+    [Parameter(Mandatory=$true)]
+    [string]$OutputDirectory,
+    
+    [Parameter(Mandatory=$true)]
+    [string]$BuildSourceDirectory,
+    
+    [Parameter(Mandatory=$true)]
+    [string]$BuildNumber,
+
+    [Parameter]
+    [string]$NuGetExe
+
+)
+    
+# get and validate the version data
+$VersionData = [regex]::Matches($BuildNumber, "\d+\.\d+\.\d+\.\d+")
+
+switch($VersionData.Count)
 {
-    throw [System.IO.FileNotFoundException] "NuGet.exe not found at $nuget"
+    0        
+        { 
+            Write-Error "Could not find version number data in BUILD_BUILDNUMBER."
+            exit 1
+        }
+    1 {}
+    default 
+        { 
+            Write-Warning "Found more than instance of version data in BUILD_BUILDNUMBER." 
+            Write-Warning "Will assume first instance is version."
+        }
+}
+$Version = $VersionData[0]
+Write-Host "Version: $Version"
+
+if ($OutputDirectory -and !(Test-Path $OutputDirectory))
+{
+    Write-Host "Output directory used but doesn't exists, creating it"
+    New-Item $OutputDirectory -type directory
 }
 
-foreach ($i in Get-Item **\*.nuspec)
+# search for nuspec files
+$NuSpecFiles = Get-Item **\*.nuspec
+
+# display found files
+Write-Host "Found nuspec files: $($NuSpecFiles.Count)"
+foreach ($NuSpecFile in $NuSpecFiles)
+{
+    Write-Host "--File: `"$NuSpecFile`""
+}
+
+# import from parameter
+$NuGetExePath = $NuGetExe
+
+# discover location of NuGet.exe from agent if not specified
+if ([string]::IsNullOrWhiteSpace($NuGetExePath))
+{
+    $NuGetExePath = "$($env:AGENT_HOMEDIRECTORY)\agent\Worker\tools\NuGet.exe"
+}
+
+# test nuget
+if (!(Test-Path $NuGetExePath))
+{
+    Write-Error "Could not locate NuGet.exe"
+    exit 1
+}
+
+# run packaging
+foreach ($i in $NuSpecFiles)
 {
     $f = [System.IO.Path]::ChangeExtension($i.FullName, ".csproj")
     if (Test-Path $f)
     {
-	    & $nuget pack $f
+	    & $NuGetExePath pack -OutputDirectory `"$OutputDirectory`" -Version `"$Version`"
     }
 }
