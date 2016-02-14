51b07b651c1ff1590f4f8a3c69cd51b7b27795e4
Jerome Haltom <wasabi@cogito.cx>
2016-02-14 13:15:04 -0600

    Added an Invoker class and Method.

---
+++ Cogito.Core/Invoker.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Runtime.ExceptionServices;
+
+namespace Cogito.Core
+{
+
+    /// <summary>
+    /// Provides methods to assist in invoking other methods.
+    /// </summary>
+    public static class Invoker
+    {
+
+        /// <summary>
+        /// Attempts to invoke the specified function, unwrapping any <see cref="AggregateException"/>s that may have occured.
+        /// </summary>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="func"></param>
+        /// <returns></returns>
+        public static TResult Try<TResult>(Func<TResult> func)
+        {
+            try
+            {
+                return func();
+            }
+            catch (AggregateException e)
+            {
+                foreach (var ie in e.Expand())
+                    ExceptionDispatchInfo.Capture(ie).Throw();
+
+                throw e;
+            }
+        }
+
+    }
+
+}
--- Cogito.Core/TryFunc.cs
+++ Cogito.Core/TryFunc.cs
@@ -1,6 +1,14 @@
 ﻿namespace Cogito
 {
 
+    /// <summary>
+    /// Describes a function which attempts to return an output value, and returns <c>true</c> if successful.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <typeparam name="TResult"></typeparam>
+    /// <param name="p1"></param>
+    /// <param name="result"></param>
+    /// <returns></returns>
     public delegate bool TryFunc<T, TResult>(T p1, out TResult result);
 
 }
