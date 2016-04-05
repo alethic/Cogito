3d4d7a1dd055081b9e9ef74d716f7d334ab88499
Jerome Haltom <wasabi@cogito.cx>
2016-04-05 13:53:27 -0500

    Removed test.

--- Cogito.Fabric.Activities.Tests/ActivityActorInstanceStoreTests.cs
+++ Cogito.Fabric.Activities.Tests/ActivityActorInstanceStoreTests.cs
@@ -38,45 +38,6 @@ namespace Cogito.Fabric.Activities.Tests
             }).Wait();
         }
 
-        [TestMethod]
-        public void Test_ActivityActorInstanceStore_Delay()
-        {
-            Task.Run(async () =>
-            {
-                var actor = new ActorStateManagerMock();
-                var state = new ActivityActorStateManager(() => actor);
-                var store = new ActivityActorInstanceStore(state);
-
-                await state.SetInstanceOwnerId(Guid.NewGuid());
-
-                var workflow = new WorkflowApplication(
-                    Cogito.Activities.Activities.Sequence(
-                        Cogito.Activities.Activities.Delay(TimeSpan.FromSeconds(5)),
-                        Cogito.Activities.Activities.Invoke(() => Console.WriteLine("End"))));
-                workflow.SynchronizationContext = new SynchronousSynchronizationContext();
-                workflow.InstanceStore = store;
-                workflow.OnUnhandledException = args =>
-                {
-                    Assert.Fail(args.UnhandledException.Message);
-                    return UnhandledExceptionAction.Abort;
-                };
-                workflow.PersistableIdle = args => PersistableIdleAction.Unload;
-                workflow.Idle = args => { };
-                await state.SetInstanceId(workflow.Id);
-
-                await workflow.RunAsync();
-
-                Assert.IsNotNull(await state.GetInstanceData<DateTime?>(ActivityWorkflowHost.ActivityTimerExpirationTimeKey));
-
-                await Task.Delay(TimeSpan.FromSeconds(7));
-
-                await workflow.RunAsync();
-                await workflow.PersistAsync();
-
-                Assert.IsNull(await state.GetInstanceData<DateTime?>(ActivityWorkflowHost.ActivityTimerExpirationTimeKey));
-            }).Wait();
-        }
-
     }
 
 }
