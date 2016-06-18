dbe59657d2f240751181bd03c5d713440a609780
Jerome Haltom <wasabi@cogito.cx>
2016-06-17 23:56:54 -0500

    Made test better.

--- Cogito.Core.Tests/ExceptionExtensionsTests.cs
+++ Cogito.Core.Tests/ExceptionExtensionsTests.cs
@@ -13,9 +13,12 @@ namespace Cogito.Core.Tests
         public void Test_expand()
         {
             var l = new List<Exception>();
-            var ae = new AggregateException(new AggregateException(new AggregateException(new Exception()), new AggregateException(new Exception())));
+            var ae = new AggregateException(new AggregateException(new AggregateException(new FormatException()), new AggregateException(new FormatException())));
             foreach (var e in ae.Expand())
+            {
+                Assert.IsTrue(e is Exception);
                 l.Add(e);
+            }
             Assert.AreEqual(2, l.Count);
         }
 
