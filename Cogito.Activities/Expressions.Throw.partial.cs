33473fdbed4beff8a0bf3628ee7dcd747c4390f0
Jerome Haltom <wasabi@cogito.cx>
2016-06-21 23:11:46 -0500

    More throw methods.

--- Cogito.Activities/Expressions.Throw.partial.cs
+++ Cogito.Activities/Expressions.Throw.partial.cs
@@ -3,6 +3,7 @@ using System.Activities;
 using System.Activities.Statements;
 using System.Diagnostics.Contracts;
 using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
 
 namespace Cogito.Activities
 {
@@ -47,6 +48,34 @@ namespace Cogito.Activities
         }
 
         /// <summary>
+        /// Throws a <typeparam name="TException"/>.
+        /// </summary>
+        /// <param name="exception"></param>
+        /// <param name="displayName"></param>
+        /// <returns></returns>
+        public static Throw Throw<TException>(Func<TException> exception, [CallerMemberName] string displayName = null)
+            where TException : Exception
+        {
+            Contract.Requires<ArgumentNullException>(exception != null);
+
+            return Throw(Invoke(exception, displayName), displayName);
+        }
+
+        /// <summary>
+        /// Throws a <typeparam name="TException"/>.
+        /// </summary>
+        /// <param name="exception"></param>
+        /// <param name="displayName"></param>
+        /// <returns></returns>
+        public static Throw Throw<TException>(Func<Task<TException>> exception, [CallerMemberName] string displayName = null)
+            where TException : Exception
+        {
+            Contract.Requires<ArgumentNullException>(exception != null);
+
+            return Throw(Invoke(exception, displayName), displayName);
+        }
+
+        /// <summary>
         /// Throws a <typeparam name="Exception"/>.
         /// </summary>
         /// <param name="exception"></param>
@@ -78,6 +107,32 @@ namespace Cogito.Activities
                 DisplayName = displayName,
                 Exception = exception,
             };
+        }
+
+        /// <summary>
+        /// Throws a <typeparam name="Exception"/>.
+        /// </summary>
+        /// <param name="exception"></param>
+        /// <param name="displayName"></param>
+        /// <returns></returns>
+        public static Throw Throw(Func<Exception> exception, [CallerMemberName] string displayName = null)
+        {
+            Contract.Requires<ArgumentNullException>(exception != null);
+
+            return Throw(Invoke(exception, displayName), displayName);
+        }
+
+        /// <summary>
+        /// Throws a <typeparam name="Exception"/>.
+        /// </summary>
+        /// <param name="exception"></param>
+        /// <param name="displayName"></param>
+        /// <returns></returns>
+        public static Throw Throw(Func<Task<Exception>> exception, [CallerMemberName] string displayName = null)
+        {
+            Contract.Requires<ArgumentNullException>(exception != null);
+
+            return Throw(Invoke(exception, displayName), displayName);
         }
 
     }
