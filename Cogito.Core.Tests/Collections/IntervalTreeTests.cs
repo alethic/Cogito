aac6df6435f6d91765ccc6a94beea1db66e2bbf6
Jerome Haltom <wasabi@cogito.cx>
2016-06-26 17:55:07 -0500

    Added an IntervalTree implementation.

---
+++ Cogito.Core.Tests/Collections/IntervalTreeTests.cs
@@ -0,0 +1,65 @@
+﻿using Cogito.Collections;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Cogito.Core.Tests.Collections
+{
+
+    [TestClass]
+    public class IntervalTreeTests
+    {
+
+        [TestMethod]
+        public void Test_basic()
+        {
+            var t = new IntervalTree<Interval<int>, int>();
+            t.Add(Interval.Create(1, 2));
+            t.Add(Interval.Create(4, 5));
+            t.Add(Interval.Create(7, 8));
+
+            Assert.IsTrue(t.FindAt(0).Length == 0);
+            Assert.IsTrue(t.FindAt(1).Length == 1);
+            Assert.IsTrue(t.FindAt(2).Length == 1);
+            Assert.IsTrue(t.FindAt(3).Length == 0);
+            Assert.IsTrue(t.FindAt(4).Length == 1);
+            Assert.IsTrue(t.FindAt(5).Length == 1);
+            Assert.IsTrue(t.FindAt(6).Length == 0);
+            Assert.IsTrue(t.FindAt(7).Length == 1);
+            Assert.IsTrue(t.FindAt(8).Length == 1);
+            Assert.IsTrue(t.FindAt(9).Length == 0);
+        }
+
+        [TestMethod]
+        public void Test_generic_interval()
+        {
+            var t = new IntervalTree<int>();
+            t.Add(0, 1);
+            var a = t.FindAt(0);
+            Assert.IsTrue(a.Length == 1);
+        }
+
+        [TestMethod]
+        public void Test_with_overlaps()
+        {
+            var t = new IntervalTree<Interval<int>, int>();
+            t.Add(Interval.Create(1, 10));
+            t.Add(Interval.Create(3, 7));
+            t.Add(Interval.Create(5, 5));
+
+            Assert.IsTrue(t.FindAt(0).Length == 0);
+            Assert.IsTrue(t.FindAt(1).Length == 1);
+            Assert.IsTrue(t.FindAt(2).Length == 1);
+            Assert.IsTrue(t.FindAt(3).Length == 2);
+            Assert.IsTrue(t.FindAt(4).Length == 2);
+            Assert.IsTrue(t.FindAt(5).Length == 3);
+            Assert.IsTrue(t.FindAt(6).Length == 2);
+            Assert.IsTrue(t.FindAt(7).Length == 2);
+            Assert.IsTrue(t.FindAt(8).Length == 1);
+            Assert.IsTrue(t.FindAt(9).Length == 1);
+            Assert.IsTrue(t.FindAt(10).Length == 1);
+            Assert.IsTrue(t.FindAt(11).Length == 0);
+        }
+
+    }
+
+}
---
+++ Cogito.Core/Collections/Interval.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+
+namespace Cogito.Collections
+{
+
+    /// <summary>
+    /// Describes an interval between two values.
+    /// </summary>
+    public abstract class Interval
+    {
+
+        /// <summary>
+        /// Creates a new <see cref="Interval"/> instance.
+        /// </summary>
+        /// <param name="start"></param>
+        /// <param name="end"></param>
+        /// <returns></returns>
+        public static Interval<TPoint> Create<TPoint>(TPoint start, TPoint end)
+            where TPoint : IComparable<TPoint>
+        {
+            Contract.Requires<ArgumentNullException>(!ReferenceEquals(start, null));
+            Contract.Requires<ArgumentNullException>(!ReferenceEquals(end, null));
+
+            return new Interval<TPoint>(start, end);
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="Interval"/> instance.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="start"></param>
+        /// <param name="end"></param>
+        /// <returns></returns>
+        public static Interval<TValue, TPoint> Create<TValue, TPoint>(TValue value, TPoint start, TPoint end)
+            where TPoint : IComparable<TPoint>
+        {
+            Contract.Requires<ArgumentNullException>(!ReferenceEquals(start, null));
+            Contract.Requires<ArgumentNullException>(!ReferenceEquals(end, null));
+
+            return new Interval<TValue, TPoint>(value, start, end);
+        }
+
+    }
+
+    /// <summary>
+    /// Generic interval between two points.
+    /// </summary>
+    /// <typeparam name="TPoint"></typeparam>
+    public class Interval<TPoint> :
+        Interval,
+        IInterval<TPoint>
+        where TPoint : IComparable<TPoint>
+    {
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="start"></param>
+        /// <param name="end"></param>
+        public Interval(TPoint start, TPoint end)
+        {
+            Contract.Requires<ArgumentNullException>(!ReferenceEquals(start, null));
+            Contract.Requires<ArgumentNullException>(!ReferenceEquals(end, null));
+
+            Start = start;
+            End = end;
+        }
+
+        /// <summary>
+        /// Gets the starting point of the <see cref="Interval{TPoint}"/>.
+        /// </summary>
+        public TPoint Start { get; private set; }
+
+        /// <summary>
+        /// Gets the ending point of the <see cref="Interval{TPoint}"/>.
+        /// </summary>
+        public TPoint End { get; private set; }
+
+    }
+
+    /// <summary>
+    /// Generic interval between two points which holds data.
+    /// </summary>
+    /// <typeparam name="TPoint"></typeparam>
+    public class Interval<TValue, TPoint> :
+        Interval<TPoint>
+        where TPoint : IComparable<TPoint>
+    {
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="start"></param>
+        /// <param name="end"></param>
+        public Interval(TValue value, TPoint start, TPoint end)
+            : base(start, end)
+        {
+            Contract.Requires<ArgumentNullException>(!ReferenceEquals(start, null));
+            Contract.Requires<ArgumentNullException>(!ReferenceEquals(end, null));
+
+            Value = value;
+        }
+
+        /// <summary>
+        /// Gets or sets the value on the interval.
+        /// </summary>
+        public TValue Value { get; set; }
+
+    }
+
+}
