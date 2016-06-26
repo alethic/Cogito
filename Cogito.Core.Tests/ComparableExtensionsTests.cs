b18b9533e87cc6761c0523ab9e0ef96084250e08
Jerome Haltom <wasabi@cogito.cx>
2016-06-26 13:51:50 -0500

    Added Comparable.Between method supporting various interval modes.

---
+++ Cogito.Core.Tests/ComparableExtensionsTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Cogito.Core.Tests
+{
+
+    [TestClass]
+    public class ComparableExtensionsTests
+    {
+
+        [TestMethod]
+        public void Test_between()
+        {
+            // test open
+            Assert.IsFalse(0.Between(1, 10, IntervalMode.Open));
+            Assert.IsTrue(1.Between(1, 10, IntervalMode.Open));
+            Assert.IsTrue(5.Between(1, 10, IntervalMode.Open));
+            Assert.IsTrue(10.Between(1, 10, IntervalMode.Open));
+            Assert.IsFalse(11.Between(1, 10, IntervalMode.Open));
+
+            // full closed
+            Assert.IsFalse(0.Between(1, 10, IntervalMode.Closed));
+            Assert.IsFalse(1.Between(1, 10, IntervalMode.Closed));
+            Assert.IsTrue(5.Between(1, 10, IntervalMode.Closed));
+            Assert.IsFalse(10.Between(1, 10, IntervalMode.Closed));
+            Assert.IsFalse(11.Between(1, 10, IntervalMode.Closed));
+
+            // left open
+            Assert.IsFalse(0.Between(1, 10, IntervalMode.SemiOpenLeft));
+            Assert.IsTrue(1.Between(1, 10, IntervalMode.SemiOpenLeft));
+            Assert.IsTrue(5.Between(1, 10, IntervalMode.SemiOpenLeft));
+            Assert.IsFalse(10.Between(1, 10, IntervalMode.SemiOpenLeft));
+            Assert.IsFalse(11.Between(1, 10, IntervalMode.SemiOpenLeft));
+
+            // right open
+            Assert.IsFalse(0.Between(1, 10, IntervalMode.SemiOpenRight));
+            Assert.IsFalse(1.Between(1, 10, IntervalMode.SemiOpenRight));
+            Assert.IsTrue(5.Between(1, 10, IntervalMode.SemiOpenRight));
+            Assert.IsTrue(10.Between(1, 10, IntervalMode.SemiOpenRight));
+            Assert.IsFalse(11.Between(1, 10, IntervalMode.SemiOpenRight));
+        }
+
+    }
+}
---
+++ Cogito.Core/IntervalMode.cs
@@ -0,0 +1,32 @@
+﻿namespace Cogito.Core
+{
+
+    /// <summary>
+    /// Specifies the type of interval comparison.
+    /// </summary>
+    public enum IntervalMode
+    {
+
+        /// <summary>
+        /// Returns <c>true</c> for values that match either left or right, or are greater than left the value on the left or less than the value on the right.
+        /// </summary>
+        Open,
+
+        /// <summary>
+        /// Returns <c>true</c> for values that are greater than the value on the left or less than the value on the right.
+        /// </summary>
+        Closed,
+
+        /// <summary>
+        /// Returns <c>true</c> for values that match the value on the left, or are greater than the value on the left or less than the value on the right.
+        /// </summary>
+        SemiOpenLeft,
+
+        /// <summary>
+        /// Returns <c>true</c> for values that match the value on the right, or are greater than the value on the left or greater than the value on the right.
+        /// </summary>
+        SemiOpenRight,
+
+    }
+
+}
