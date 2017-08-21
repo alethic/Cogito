49e19b7d41d0b32b494950223679da1fbe9d6f42
Jerome Haltom <wasabi@cogito.cx>
2017-08-20 23:54:07 -0500

    Converted to Sdk project.

--- Cogito.Core/Properties/AssemblyInfo.cs
+++ Cogito.Core/Properties/AssemblyInfo.cs
@@ -1,21 +1,4 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Cogito.Core")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Cogito.Core")]
-[assembly: AssemblyCopyright("Copyright © Jerome Haltom 2016")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-[assembly: ComVisible(false)]
-[assembly: Guid("e6c32b1b-b855-40a8-8456-2e1d0697c183")]
-
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+﻿using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Cogito.Core.Tests")]
 [assembly: InternalsVisibleTo("Cogito.Composition")]
