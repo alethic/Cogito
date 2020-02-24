268379bdb8118bd26536d4907a6be53546459dab
Jerome Haltom <wasabi@cogito.cx>
2020-02-23 23:23:16 -0600

    Added async disposable and cancellation to AsyncLock.

--- Cogito.Core/Threading/AsyncLock.cs
+++ Cogito.Core/Threading/AsyncLock.cs
@@ -8,8 +8,47 @@ namespace Cogito.Threading
     public class AsyncLock
     {
 
-        struct Releaser :
-            IDisposable
+#if NETSTANDARD2_1 || NETSTANDARD2_0
+
+        struct AsyncReleaser : IAsyncDisposable, IDisposable
+        {
+
+            readonly AsyncLock lck;
+
+            /// <summary>
+            /// Initializes a new instance.
+            /// </summary>
+            /// <param name="lck"></param>
+            internal AsyncReleaser(AsyncLock lck)
+            {
+                this.lck = lck;
+            }
+
+            /// <summary>
+            /// Disposes of the instance.
+            /// </summary>
+            public ValueTask DisposeAsync()
+            {
+                if (lck != null)
+                    lck.semaphore.Release();
+
+                return new ValueTask(Task.CompletedTask);
+            }
+
+            /// <summary>
+            /// Disposes of the instance.
+            /// </summary>
+            public void Dispose()
+            {
+                if (lck != null)
+                    lck.semaphore.Release();
+            }
+
+        }
+
+#else
+
+        struct Releaser : IDisposable
         {
 
             readonly AsyncLock lck;
@@ -31,8 +70,55 @@ namespace Cogito.Threading
 
         }
 
+#endif
+
         readonly SemaphoreSlim semaphore;
+
+#if NETSTANDARD2_1 || NETSTANDARD2_0
+        readonly Task<IAsyncDisposable> lck;
+#else
         readonly Task<IDisposable> lck;
+#endif
+
+#if NETSTANDARD2_1 || NETSTANDARD2_0
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        public AsyncLock()
+        {
+            semaphore = new SemaphoreSlim(1);
+            lck = Task.FromResult<IAsyncDisposable>(new AsyncReleaser(this));
+        }
+
+        /// <summary>
+        /// Creates a task which resolves when the lock is free. Dispose of the resulting instance to release the lock.
+        /// </summary>
+        /// <returns></returns>
+        public Task<IAsyncDisposable> LockAsync(CancellationToken cancellationToken = default)
+        {
+            var wait = semaphore.WaitAsync(cancellationToken);
+            if (wait.IsCompleted)
+                return lck;
+            else
+                return wait.ContinueWith((_, state) =>
+                    (IAsyncDisposable)new AsyncReleaser((AsyncLock)state),
+                    this,
+                    CancellationToken.None,
+                    TaskContinuationOptions.ExecuteSynchronously,
+                    TaskScheduler.Default);
+        }
+
+        /// <summary>
+        /// Creates a task which resolves when the lock is free. Dispose of the resulting instance to release the lock.
+        /// </summary>
+        /// <returns></returns>
+        public Task<IAsyncDisposable> LockAsync()
+        {
+            return LockAsync(CancellationToken.None);
+        }
+
+#else
 
         /// <summary>
         /// Initializes a new instance.
@@ -47,9 +133,9 @@ namespace Cogito.Threading
         /// Creates a task which resolves when the lock is free. Dispose of the resulting instance to release the lock.
         /// </summary>
         /// <returns></returns>
-        public Task<IDisposable> LockAsync()
+        public Task<IDisposable> LockAsync(CancellationToken cancellationToken = default)
         {
-            var wait = semaphore.WaitAsync();
+            var wait = semaphore.WaitAsync(cancellationToken);
             if (wait.IsCompleted)
                 return lck;
             else
@@ -60,6 +146,17 @@ namespace Cogito.Threading
                     TaskContinuationOptions.ExecuteSynchronously,
                     TaskScheduler.Default);
         }
+        
+        /// <summary>
+        /// Creates a task which resolves when the lock is free. Dispose of the resulting instance to release the lock.
+        /// </summary>
+        /// <returns></returns>
+        public Task<IDisposable> LockAsync()
+        {
+            return LockAsync(CancellationToken.None);
+        }
+
+#endif
 
     }
 
